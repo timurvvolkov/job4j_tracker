f058e6325e30c02404cac2597bc89d7e63540524
timurvvolkov <timurvvolkov@gmail.com>
2020-04-15 11:35:02 +0300

    9. Написать тесты на StartUI.[#256819]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,9 +1,6 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
-
-import javax.rmi.CORBA.Stub;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -42,5 +39,15 @@ public class StartUITest {
         new DeleteAction().execute(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
         assertThat(deleted == null, is(true));
+    }
+
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
     }
 }
