7655a7a64e4737a9b04618f9efe1211365f3371f
timurvvolkov <timurvvolkov@gmail.com>
2020-04-20 14:50:40 +0300

    1.1. Тесты на ValidateInput.[#256831]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ValidateInputTest {
+    @Test
+    public void whenInvalidInput() {
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(mem));
+        String[] data = {"one", "1"};
+        ValidateInput input = new ValidateStubInput(data);
+        input.askInt("Enter");
+        assertThat(
+                mem,
+                is(String.format("Please enter validate data again.%n"))
+        );
+        System.setOut(out);
+    }
+}
