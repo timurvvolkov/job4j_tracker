a780ed87553689273a3f5f2e6b657f5729587c13
timurvvolkov <timurvvolkov@gmail.com>
2020-04-30 16:03:09 +0300

    6. Изменить программу Tracker из 2-го модуля[#256791]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,34 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StartUI {
-//    public void init(Input input, Tracker tracker, UserAction[] actions) {
-//        boolean run = true;
-//        while (run) {
-//            this.showMenu(actions);
-//            int select = input.askInt("Select: ", actions.length);
-//            UserAction action = actions[select];
-//            run = action.execute(input, tracker);
-//        }
-//    }
-//
-//    private void showMenu(UserAction[] actions) {
-//        System.out.println("Menu.");
-//        for (int index = 0; index < actions.length; index++) {
-//            System.out.println(index + ". " + actions[index].name());
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        Input input = new ConsoleInput();
-//        Input validate = new ValidateInput(input);
-//        Tracker tracker = new Tracker();
-//        UserAction[] actions = {
-//                new CreateAction(), new FindAllAction(), new ReplaceAction(), new DeleteAction(),
-//                new FindIdAction(), new FindByNameAction(), new ExitAction()
-//        };
-//        new StartUI().init(validate, tracker, actions);
-//    }
-
     public void init(Input input, Tracker tracker, List<UserAction> actions) {
         boolean run = true;
         while (run) {
